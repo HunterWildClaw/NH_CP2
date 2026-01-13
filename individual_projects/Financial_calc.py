4bcb7b03fc4e1fec7162303ac2fc2828ffb1c53f
HunterWildClaw <nathan.hasson@ucas-edu.net>
2026-01-13 09:57:59 -0700

    ughhh

--- individual_projects/Financial_calc.py
+++ individual_projects/Financial_calc.py
@@ -4,15 +4,20 @@ import math
 
 #Ask user if they wanna do Compound Interest Calculator, Budget Allocator, Sale Price Calculaton, or Tip Calculator
 def financial_calc():
-    user_choice=input("Would you like to use the \n1) Compound Interest Calculator \n2) Budget Allocator\n3) Sale Price Calculaton\n4)Savings Time Claculator\n5) Tip Calculator\nEnter number here: ").strip()
-    #If user trolled
-    #if the user picked 1:
-    if user_choice=='1':
-        starting_ammount=int(input("What's your starting ammount?: "))
-        interest_rate_percent=int(input("And what's the interest rate percent?: "))
-        years_spent_compounding=int(input("And finally, how many years will you spend compunding?: "))
-        for i in years_spent_compounding:
-            new_amount=starting_ammount*(interest_rate_percent/100+1)
-        print(f"After {years_spent_compounding}, with an interest rate of {interest_rate_percent}, your starting ammount of ${starting_ammount} will become ${new_amount}!")
-        
+    while True:
+        user_choice=input("Would you like to use the \n1) Compound Interest Calculator \n2) Budget Allocator\n3) Sale Price Calculaton\n4)Savings Time Claculator\n5) Tip Calculator\nEnter number here: ").strip()
+        #if the user picked 1:
+        if user_choice=='1':
+            starting_ammount=int(input("What's your starting ammount?: "))
+            interest_rate_percent=int(input("And what's the interest rate percent?: "))
+            years_spent_compounding=int(input("And finally, how many years will you spend compunding?: "))
+            starting_ammount_percentage=starting_ammount*-100
+            for i in range(years_spent_compounding):
+                new_amount=starting_ammount*(interest_rate_percent/100+1)
+            print(f"After {years_spent_compounding}, with an interest rate of {interest_rate_percent}, your starting ammount of ${starting_ammount} will become ${new_amount}!")
+            
+        #If user trolled
+
+
+
 financial_calc()
