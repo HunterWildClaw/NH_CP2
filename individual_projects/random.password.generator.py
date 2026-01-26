a6536d17410e5aea319fc0a74cf1e1ba9494652c
HunterWildClaw <nathan.hasson@ucas-edu.net>
2026-01-26 10:51:04 -0700

    I DONT LIKE THIS GAME ANYMOER!

--- individual_projects/random.password.generator.py
+++ individual_projects/random.password.generator.py
@@ -1,16 +1,36 @@
 # NH 2nd Random Password Generator
+#import random as r
+import random as r
+
+#Make the things that hold all the characters
+caps_letters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower_letters= "abcdefghijklmnopqrstuvwxyz"
+nums=[1,2,3,4,5,6,7,8,9]
+special_chars="!@#$%^&*()¡™£¢∞§¶•ªº⁄€‹›ﬁﬂ‡°·‚œ∑´®†¥¨ˆøπåß∂ƒ¬…“‘«÷≥≤µ˜∫√ç≈ΩŒ„´‰ˇÁ¨ˆØ∏”’ÅÍÍÎÏ˝ÓÔÒÚÆ»`¸˛Ç◊ı˜Â¯˘¿Ω=-+_≠–±—"
+
+def get_input(prompt):
+    while True:
+        answer=input(prompt)
+        if answer == "1":
+            return True
+        if answer == "2":
+            return False
+        print("Invalid input. Try again")
+
+
+def info_getter_and_password_generator():
+    password_length=get_input("How long would you like your password to be? Please give a number: ").strip()
+    password_lower=get_input("Does it need lowercase letters? '1' for yes and '2' for no: ").strip()
+    password_upper=get_input("Does it need uppercase letters? '1' for yes and '2' for no: ").strip()
+    password_number=get_input("Does the password need numbers? '1' for yes and '2' for no: ").strip()
+    password_special_chars=get_input("Does the password need special characters? '1' for yes and '2' for no: ").strip()
 
 def main():
-    print("Hello and welcome to my Random Password Generator!")
-    user_first_choice=input("Would you like to get generating or did you make a mistake and want to leave? Select '1' to start generating a password or '2' to leave ")
-    if user_first_choice == '1':
-        password_length=int(input("Great! Now how long would like your password to be?"))
-        password_lower=int(input("Does it need lowercase letters? 1 for yes and 2 for no"))
-        password_upper=int(input("Does it need uppercase letters? 1 for yes and 2 for no"))
-        password_number=int(input("Does it need numbers? 1 for yes and 2 for no"))
-        password_special_char=int(input("Do you want special characters? 1 for yes and 2 for no"))
-        print("Here are the available passwords:")
-
-
+    while True:
+        print("Hello and welcome to my Random Password Generator!")
+        user_first_choice=get_input("Would you like to make a password? '1' for yes and '2' for no: ")
+        if user_first_choice=='1':
+            info_getter_and_password_generator()
+        
 
 main()
