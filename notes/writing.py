952505268cb93aaf202219853a51e2e8e9b8a043
HunterWildClaw <nathan.hasson@ucas-edu.net>
2026-02-09 10:50:22 -0700

    ugh i hate coding

---
+++ notes/writing.py
@@ -0,0 +1,37 @@
+
+"""with open("notes\\sample.txt", 'a') as file:
+    file.write("\nJoe\n")
+    file.write("Israel\n")
+    file.write("Zee\n")
+
+print("Run finished")"""
+"""content = []
+with open("notes/sample.txt", 'r+') as file:
+    for line in file:
+        content.append(line.strip())
+        
+    index = content.index('Tia')
+    content[index] = "Torii"
+
+    file.truncate(0)
+    for name in content:
+        file.write(name + "\n")
+
+print("Code ends")"""
+
+
+import csv
+"""with open("notes/test.csv", 'w', newline='') as csvfile:
+    fieldnames = ['name', 'Radiant']
+    writer = csv.writer(csvfile)
+
+    #writer.writerow(fieldnames)
+    writer.writerow(["name1", "edge_dancer"])
+    writer.writerow(["name2", "truth_watcher"])"""
+with open("notes/test.csv", 'a', newline='') as csvfile:
+    fieldnames = ['name', 'Radiant']
+    writer = csv.writer(csvfile)
+
+    #writer.writerow(fieldnames)
+    writer.writerow(["name1", "edge_dancer"])
+    writer.writerow(["name2", "truth_watcher"])
